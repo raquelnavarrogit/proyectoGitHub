697872ed98ae6747aa4a1b422f7c2830debb102f
raquelnavarrogit <159776474+raquelnavarrogit@users.noreply.github.com>
2024-03-21 13:39:04 +0100

    Get y Set nombre en Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -16,6 +16,18 @@ public class Cliente {
 		this.dni = dni;
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	
+	
+
+	
 	@Override
 	public String toString() {
 		return "Cliente [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + "]";
