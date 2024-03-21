dae66f021c12c2a981d645f7cb306f2afe8cf19f
raquelnavarrogit <159776474+raquelnavarrogit@users.noreply.github.com>
2024-03-21 13:59:33 +0100

    Modificación constructor y método ToString Producto.java

--- Producto.java
+++ Producto.java
@@ -8,18 +8,19 @@ public class Producto {
 	private int cantidad;
 	private String color;
 	
-	public Producto(int id, String nombre, String marca, int cantidad) {
+	public Producto(int id, String nombre, String marca, int cantidad, String color) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
 		this.marca = marca;
 		this.cantidad = cantidad;
+		this.color = color;
 	}
 
 
 	@Override
 	public String toString() {
-		return "Producto [id=" + id + ", nombre=" + nombre + ", marca=" + marca + ", cantidad=" + cantidad + "]";
+		return "Producto [id=" + id + ", nombre=" + nombre + ", marca=" + marca + ", cantidad=" + cantidad + ",color=" + color + "]";
 	}
 	
 	
