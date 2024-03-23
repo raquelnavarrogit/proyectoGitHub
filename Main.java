47c6c479a24b7c308c51c27fcbcf5322fb5bf521
raquelnavarrogit <159776474+raquelnavarrogit@users.noreply.github.com>
2024-03-23 12:23:28 +0100

    Syso de prueba en Main.java

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class MainTienda {
 		teashop.mostrarClientes();
 		teashop.mostrarProductos();
 
-		
+		System.out.println("Aquí estamos probando la nueva rama que hemos creado");
 		
 		
 		System.out.println("Esto es una prueba de git");
