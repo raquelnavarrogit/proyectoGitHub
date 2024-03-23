d555ee9092ef6f92df9cf8bc9657298df8bb84e0
raquelnavarrogit <159776474+raquelnavarrogit@users.noreply.github.com>
2024-03-23 10:29:20 +0100

    Get and Set cantidad en Producto.java

--- Producto.java
+++ Producto.java
@@ -33,6 +33,14 @@ public class Producto {
 		this.marca = marca;
 	}
 
+	public int getCantidad() {
+		return cantidad;
+	}
+		
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
 	public String getColor() {
 		return color;
 	}
